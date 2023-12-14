801a2e67d3cc3beaee3ee99df981b89fded8fedf
Angel Chavez <angel.chavez@iherb.com>
2023-12-14 15:17:31 -0700

    changes for hosting on render website

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const forecast = require(path.join(__dirname, './utils/forecast'))
 const geocode = require(path.join(__dirname, './utils/geocode'))
 
 const app = express()
+const port = process.env.PORT || 3000
+
 
 //Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -110,6 +112,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}.`)
 })
